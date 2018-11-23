21b2e92e41ea996bdc1fa1466b06e892ef112a13
Oleksandr Fedorov <a.g.fedorof@gmail.com>
2018-11-23 03:17:58 +0200

    Updates

---
+++ precache-manifest.b89a0ddbf62e3cfa2d0bccfca64f9681.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c37fc8ce3edd8bf01a8e",
+    "url": "/licserver/static/css/main.f88c26f2.chunk.css"
+  },
+  {
+    "revision": "c37fc8ce3edd8bf01a8e",
+    "url": "/licserver/static/js/main.c37fc8ce.chunk.js"
+  },
+  {
+    "revision": "9ff49331fe2a2d1d1996",
+    "url": "/licserver/static/js/1.9ff49331.chunk.js"
+  },
+  {
+    "revision": "71651050c602412e5261",
+    "url": "/licserver/static/js/runtime~main.71651050.js"
+  },
+  {
+    "revision": "65867398dd462d60afddd2240dec816f",
+    "url": "/licserver/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.06e02b0d7d636cb0cee02f5706312e2e.js"
+  "/licserver/precache-manifest.b89a0ddbf62e3cfa2d0bccfca64f9681.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/licserver/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
