b5721f2581c0bf5e63cc62b47646659ee00dda68
Oleksandr Fedorov <a.g.fedorof@gmail.com>
2018-11-23 03:15:50 +0200

    Updates

---
+++ precache-manifest.06e02b0d7d636cb0cee02f5706312e2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4542ce5b4b853c8b41c",
+    "url": "/static/css/main.f88c26f2.chunk.css"
+  },
+  {
+    "revision": "c4542ce5b4b853c8b41c",
+    "url": "/static/js/main.c4542ce5.chunk.js"
+  },
+  {
+    "revision": "cfff5f6c777dcce11f6f",
+    "url": "/static/js/1.cfff5f6c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "00a1b7a3eb21ffa764e766354044ca11",
+    "url": "/index.html"
+  }
+];
